db2865c9f021b59516c382aeab04de73dacaf5b6
vinitraj10 <ent.vinit01@gmail.com>
2017-05-07 14:59:04 +0530

    doubly and circular linklist added

---
+++ Link-Lists/python/doublylinklist.py
@@ -0,0 +1,35 @@
+class Node:
+	def __init__(self,value):
+		self.value=value
+		self.next=None
+		self.prev=None
+
+
+
+a=Node(1)
+b=Node(2)
+c=Node(3)
+
+#doubly link list creation:-
+
+'''a.next=b
+b.prev=a
+b.next=c
+c.prev=b'''
+
+
+#circular linklist
+a.next=b
+b.next=c
+c.next=a
+
+item =a
+while item is not None:
+	print(item.value)
+	item=item.next
+	if item is a:
+		print(item.value)
+		break
+
+#a->
+
