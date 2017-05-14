a9b031e64c2925e89305a78c1739f4f881c42597
vinitraj10 <ent.vinit01@gmail.com>
2017-05-14 12:23:44 +0530

    Added deletion.py file with code updates

--- Link-Lists/python/deletion.py
+++ Link-Lists/python/deletion.py
@@ -1,51 +1,58 @@
 class Node:
-	def __init__(self,value):
-		self.value=value
+	def __init__(self,data):
+		self.data=data
 		self.next=None
 
-def show(head):
-	item=head
-	while item is not None:
-		if item.next is not None:
-			print(item.value,end=' ->')
-		else:
-			print(item.value)
-		item=item.next
 
-def delete(head,d):
-	if d==1:
-		item=head
-		head=item.next
-		return head
-	elif d==N:
-		item=head
-		for i in range(n-1):
+class LinkedList:
+	def __init__(self):
+		self.head=None
+	def show(self):
+		item=self.head
+		while item is not None:
+			if item.next is None:
+				print(item.data)
+			else:
+				print(item.data,end='->')
+			item=item.next
+	def delFirst(self):
+		item=self.head
+		self.head=item.next
+	def delLast(self):
+		item=self.head
+		while item.next.next is not None:
 			item=item.next
 		item.next=None
-		return head
-	else:
-		item=head
-		for i in range(1,d-1):
+	def delNode(self,n):
+		item=self.head
+		for i in range(n-2):
 			item=item.next
 		item.next=item.next.next
-		return head
 
 
-N=int(input('Enter The intial Size of The list\n'))
-print('Enter Head\n')
-x=int(input())
-p=Node(x)
-item=p
-print('Enter the list inputs now:-')
-while N>1:
-	k=int(input())
-	q=Node(k)
-	item.next=q
-	q.next=None
-	item=q
-	N=N-1
-
-print('Enter the Node No you want to delete:\n')
-d=int(input())
-x=delete(p,d) #Returns new head after deletion
-show(x)
+if __name__ == '__main__':
+	llist=LinkedList()
+	llist.head=Node(1)
+	a=Node(2)
+	b=Node(3)
+	c=Node(4)
+	d=Node(5)
+	e=Node(6)
+	f=Node(7)
+	llist.head.next=a
+	a.next=b
+	b.next=c
+	c.next=d
+	d.next=e
+	e.next=f
+	llist.show()
+	llist.delFirst()
+	llist.show()
+	llist.delLast()
+	llist.show()
+	llist.delNode(4)
+	llist.show()
+	llist.delNode(2)
+	llist.show()	
+	llist.delNode(3)
+	llist.show()
